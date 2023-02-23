33a4dd7bfef2160f923573f88120065ed1897a55
KeElLiao <126189902+KeElLiao@users.noreply.github.com>
2023-02-23 15:59:50 -0500

    Add files via upload

---
+++ lab 3/1st part/q1 javascript.js
@@ -0,0 +1,23 @@
+function validateForm() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "" || !/^[a-zA-Z0-9]+$/.test(username)) {
+      console.log("Form submission failed: Invalid username");
+      return false;
+    }
+
+    if (email === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      console.log("Form submission failed: Invalid email");
+      return false;
+    }
+  
+    if (password === "" || password.length < 8) {
+      console.log("Form submission failed: Invalid password");
+      return false;
+    }
+  
+    console.log("Form submitted successfully");
+    return true;
+  }
